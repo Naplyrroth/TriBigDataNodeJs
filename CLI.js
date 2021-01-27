29e042c68ce49b6d3057f38a14a919b5bcb8f972
Ugo <ugo.prenat@gmail.com>
2021-01-27 13:47:49 +0100

    Story 1 finie => fonction transform + message d'erreur CLI

--- CLI.js
+++ CLI.js
@@ -9,24 +9,38 @@ for (let i = 2; i < args.length; i++) {
     const arg = args[i];
 
     if(arg == '-action') {
-        if (args[i+1] == 'transform') {
+        if (args[i+1] == 'transform' && args.length > i+3) {
             transform(args[i+2], args[i+3])
+            break
         }
-        else if (args[i+1] == 'sort_date') {
+        else if (args[i+1] == 'sort_date' && args.length > i+3) {
             sort.sortDate(args[i+2], args[i+3])
+            break
         }
-        else if (args[i+1] == 'sort_title') {
+        else if (args[i+1] == 'sort_title' && args.length > i+3) {
             sort.sortTitle(args[i+2], args[i+3])
+            break
         }
-        else if (args[i+1] == 'search_date') {
+        else if (args[i+1] == 'search_date' && args.length > i+4) {
             date.searchDate(args[i+2], args[i+3], args[i+4])
+            break
         }
-        else if (args[i+1] == 'search_key_word') {
+        else if (args[i+1] == 'search_key_word' && args.length > i+4) {
             search.searchKeyWord(args[i+2], args[i+3], args[i+4])
+            break
+        }
+        else {
+            console.log("\nCommande invalide\n");
+            break
         }
     }
-    else if (arg == '-save') {
+    else if (arg == '-save' && args.length > i+1) {
         img.saveImg(args[i+1])
+        break
+    }
+    else {
+        console.log("\nCommande invalide\n");
+        break
     }
 }
 
--- transform.js
+++ transform.js
@@ -1,5 +1,16 @@
+const fs = require('fs')
+
 module.exports = (inputFile, outputFile) => {
-    console.log('Transform function');
-    console.log(`Inputfile: ${inputFile}`);
-    console.log(`Ourputfile: ${outputFile}`);
+    // Get the data of the input file
+    let readFile = fs.readFileSync(inputFile)
+    let data = JSON.parse(readFile)
+
+    // Set the new title for each movie
+    for (let i = 0; i < data.length; i++) {
+        const movie = data[i];
+        const releaseDate = new Date(movie.release_date * 1000)
+        movie.title += ` ${releaseDate.getFullYear()}`
+    }
+    // Create the new file
+    fs.writeFileSync(outputFile, JSON.stringify(data))
 }
