a1ebdd8c39598d15933222b4f0322e07df40dcbd
Mathis Ouchikh <mathisouchikh@gmail.com>
2021-01-28 15:11:14 +0100

    Update img.js

--- img.js
+++ img.js
@@ -1,12 +1,11 @@
-const fs= require('fs')
-const request= require('request')
-
 module.exports = {
     saveImg: (folderPath) => {
         console.log('Save image function');
         console.log(`Folder path: ${folderPath}`)
     }
 }
+const fs= require('fs')
+const request= require('request')
 
 const download = (url, path, callback) => {
     request.head(url, (err, res, body) => {
@@ -15,8 +14,11 @@ const download = (url, path, callback) => {
         .on('close', callback)
     })
 }
-const url = 'https://image.tmdb.org/t/p/w500/reNPMjkPg9f6wgf6kHSSkKjBarL.jpg'
-const path= './img'
+const url = 'https://image.tmdb.org/t/p/w500/f1IFS23zAXRUsRa35JuEuLHZtSV.jpg'
+const path= 'G:\TriBigDataNodeJs\Img'
 download(url, path, () => {
     console.log('Done!')
 })
+
+
+
