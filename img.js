6dd0b97be3072e130529a753f2f7c10f8a566a94
Ugo <ugo.prenat@gmail.com>
2021-01-28 14:39:20 +0100

    bugs sauvegarde d'images

--- img.js
+++ img.js
@@ -1,24 +1,22 @@
-/* module.exports = {
+const fs= require('fs')
+const request= require('request')
+
+module.exports = {
     saveImg: (folderPath) => {
         console.log('Save image function');
         console.log(`Folder path: ${folderPath}`)
     }
 }
-const fs= require('fs')
-const request= require('request')
 
-constdownload = (url, path, callback) => {
+const download = (url, path, callback) => {
     request.head(url, (err, res, body) => {
         request(url)
         .pipe(fs.createWriteStream(path))
         .on('close', callback)
     })
 }
-consturl = 'https://image.tmdb.org/t/p/w500/reNPMjkPg9f6wgf6kHSSkKjBarL.jpg'
-constpath= 'G:\TriBigDataNodeJs\Img'
+const url = 'https://image.tmdb.org/t/p/w500/reNPMjkPg9f6wgf6kHSSkKjBarL.jpg'
+const path= './img'
 download(url, path, () => {
     console.log('Done!')
 })
-
-
-
