f41402264c084d24c3c379896f2d5c5cea0f3b58
Ugo <ugo.prenat@gmail.com>
2021-01-28 15:05:33 +0100

    changements Story 6 => sauvegarde d'image

--- img.js
+++ img.js
@@ -4,21 +4,22 @@ const request= require('request')
 module.exports = {
     saveImg: (folderPath) => {
 
-        let url = 'https://image.tmdb.org/t/p/w500/reNPMjkPg9f6wgf6kHSSkKjBarL.jpg'
-        let filename = 'Dragon Ball GT poster.jpg'
-          
-        download(url, filename, function(){
-            console.log('done')
-        })
-
-        let download = function(url, filename, callback){
+        let download = function(url, filename, folderPath, callback){
             request.head(url, function(err, res, body){
                 console.log('content-type:', res.headers['content-type'])
                 console.log('content-length:', res.headers['content-length'])
           
-                request(url).pipe(fs.createWriteStream(filename)).on('close', callback)
+                request(url).pipe(fs.createWriteStream(folderPath + '/' + filename)).on('close', callback)
             })
         }
+        
+        let url = 'https://image.tmdb.org/t/p/w500/reNPMjkPg9f6wgf6kHSSkKjBarL.jpg'
+        let filename = 'Dragon Ball GT poster.jpg'
+          
+        download(url, filename, folderPath, function(){
+            console.log('done')
+        })
+
     }
 }
 
